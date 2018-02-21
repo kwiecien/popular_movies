4c383ad2e160451cf12eefb6ac9987134b84d1e6
kwiecien <k.d.kwiecinski@gmail.com>
2018-02-21 20:11:03 +0100

    refactor: Rename activity_movie_posters layout

--- app/src/main/java/com/kk/popularmovies/MoviePostersActivity.java
+++ app/src/main/java/com/kk/popularmovies/MoviePostersActivity.java
@@ -8,6 +8,6 @@ public class MoviePostersActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_movie_posters);
     }
 }
