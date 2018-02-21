aa56ee2305841fd3e40b483c4e3e51dc52fbb1f3
kwiecien <k.d.kwiecinski@gmail.com>
2018-02-21 20:08:09 +0100

    feat: Add MovieDetails activity and rename MainActivity to MoviePosters

---
+++ app/src/main/java/com/kk/popularmovies/MovieDetailsActivity.java
@@ -0,0 +1,13 @@
+package com.kk.popularmovies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MovieDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_details);
+    }
+}
--- app/src/main/java/com/kk/popularmovies/MoviePostersActivity.java
+++ app/src/main/java/com/kk/popularmovies/MoviePostersActivity.java
@@ -3,7 +3,7 @@ package com.kk.popularmovies;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MoviePostersActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
