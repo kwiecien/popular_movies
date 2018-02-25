ce2dfedd7ba6c9b7aa155be45aa9010e82fc4875
kwiecien <k.d.kwiecinski@gmail.com>
2018-02-25 22:42:03 +0100

    refactor: Change from GridView to RecyclerView

--- app/src/main/java/com/kk/popularmovies/MoviePostersFragment.java
+++ app/src/main/java/com/kk/popularmovies/MoviePostersFragment.java
@@ -4,30 +4,17 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.GridView;
+import android.widget.Toast;
 
-import com.kk.popularmovies.model.Movie;
-
-import java.time.LocalDate;
-import java.util.Arrays;
-
-public class MoviePostersFragment extends Fragment {
+public class MoviePostersFragment extends Fragment implements MoviesAdapter.MoviesAdapterOnClickHandler {
 
     private MoviesAdapter mMoviesAdapter;
-
-    private Movie[] mMovies = {
-            new Movie.Builder("Test0", LocalDate.now()).build(),
-            new Movie.Builder("Test1", LocalDate.now()).build(),
-            new Movie.Builder("Test2", LocalDate.now()).build(),
-            new Movie.Builder("Test3", LocalDate.now()).build(),
-            new Movie.Builder("Test4", LocalDate.now()).build(),
-            new Movie.Builder("Test5", LocalDate.now()).build(),
-            new Movie.Builder("Test6", LocalDate.now()).build(),
-            new Movie.Builder("Test7", LocalDate.now()).build(),
-    };
+    private RecyclerView mRecyclerView;
 
     public MoviePostersFragment() {
     }
@@ -36,9 +23,21 @@ public class MoviePostersFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_movie_posters, container, false);
-        mMoviesAdapter = new MoviesAdapter(getActivity(), Arrays.asList(mMovies));
-        GridView gridView = rootView.findViewById(R.id.movies_gv);
-        gridView.setAdapter(mMoviesAdapter);
+
+        mRecyclerView = rootView.findViewById(R.id.rv_movies);
+        int spanCount = 2;
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getActivity(), spanCount);
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setHasFixedSize(true);
+
+        mMoviesAdapter = new MoviesAdapter(this);
+        mRecyclerView.setAdapter(mMoviesAdapter);
+
         return rootView;
+    }
+
+    @Override
+    public void onClick() {
+        Toast.makeText(getActivity(), "Clicked!", Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/kk/popularmovies/MoviesAdapter.java
+++ app/src/main/java/com/kk/popularmovies/MoviesAdapter.java
@@ -1,41 +1,87 @@
 package com.kk.popularmovies;
 
-import android.app.Activity;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.kk.popularmovies.model.Movie;
 
-import java.util.List;
+import java.time.LocalDate;
 
-public class MoviesAdapter extends ArrayAdapter<Movie> {
+public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MoviesAdapterViewHolder> {
 
-    public MoviesAdapter(Activity context, List<Movie> movies) {
-        super(context, 0, movies);
+    private final MoviesAdapterOnClickHandler mClickHandler;
+    private Movie[] mMovies = {
+            new Movie.Builder("Test0", LocalDate.now()).build(),
+            new Movie.Builder("Test1", LocalDate.now()).build(),
+            new Movie.Builder("Test2", LocalDate.now()).build(),
+            new Movie.Builder("Test3", LocalDate.now()).build(),
+            new Movie.Builder("Test4", LocalDate.now()).build(),
+            new Movie.Builder("Test5", LocalDate.now()).build(),
+            new Movie.Builder("Test6", LocalDate.now()).build(),
+            new Movie.Builder("Test7", LocalDate.now()).build(),
+    };
+
+    public MoviesAdapter(MoviesAdapterOnClickHandler clickHandler) {
+        mClickHandler = clickHandler;
     }
 
-    @NonNull
     @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item_movie, parent, false);
+    public MoviesAdapterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        int layoutIdForListItem = R.layout.movie_list_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+        View view = inflater.inflate(layoutIdForListItem, parent, shouldAttachToParentImmediately);
+        return new MoviesAdapterViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MoviesAdapterViewHolder moviesAdapterViewHolder, int position) {
+        moviesAdapterViewHolder.bind(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mMovies == null) {
+            return 0;
         }
-        Movie movie = getItem(position);
+        return mMovies.length;
+    }
 
-        ImageView posterIv = convertView.findViewById(R.id.list_item_poster_iv);
-        int imageThumbnail = movie.getImageThumbnail();
-        posterIv.setImageResource(imageThumbnail); // TODO: Correct way of handling imageThumbnails
+    public interface MoviesAdapterOnClickHandler {
+        void onClick();
+    }
 
-        TextView titleTv = convertView.findViewById(R.id.list_item_title_tv);
-        String title = movie.getTitle();
-        titleTv.setText(title);
+    public class MoviesAdapterViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public final ImageView mPosterIv;
+        public final TextView mTitleTv;
 
-        return convertView;
+        public MoviesAdapterViewHolder(View itemView) {
+            super(itemView);
+            mPosterIv = itemView.findViewById(R.id.list_item_poster_iv);
+            mTitleTv = itemView.findViewById(R.id.list_item_title_tv);
+            itemView.setOnClickListener(this);
+        }
+
+        public void bind(int position) {
+            Movie movie = mMovies[position];
+
+            int imageThumbnail = movie.getImageThumbnail();
+            mPosterIv.setImageResource(imageThumbnail); // TODO: Correct way of handling imageThumbnails
+
+            String title = movie.getTitle();
+            mTitleTv.setText(title);
+        }
+
+        @Override
+        public void onClick(View view) {
+            int adapterPosition = getAdapterPosition();
+            mClickHandler.onClick();
+        }
     }
 }
