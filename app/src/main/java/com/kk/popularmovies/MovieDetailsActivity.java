94d64c2b04f09a4210ac5444e72d9cc8a6abf961
kwiecien <k.d.kwiecinski@gmail.com>
2018-02-27 19:09:55 +0100

    feat: Display background image for MovieDetails

--- app/src/main/java/com/kk/popularmovies/MovieDetailsActivity.java
+++ app/src/main/java/com/kk/popularmovies/MovieDetailsActivity.java
@@ -4,9 +4,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.kk.popularmovies.model.Movie;
+import com.squareup.picasso.Picasso;
+
+import java.util.Locale;
 
 public class MovieDetailsActivity extends AppCompatActivity {
 
@@ -32,7 +36,13 @@ public class MovieDetailsActivity extends AppCompatActivity {
         TextView plotSynopsisTv = findViewById(R.id.movie_details_plot_synopsis_tv);
         movieTv.setText(movie.getTitle());
         releaseDateTv.setText(movie.getReleaseDate().toString());
-        userRankingTv.setText(Double.toString(movie.getUserRating()));
+        userRankingTv.setText(String.format(Locale.getDefault(), "%1.2f", movie.getUserRating()));
         plotSynopsisTv.setText(movie.getPlotSynopsis());
+        String imageThumbnail = movie.getImageThumbnail();
+        ImageView backgroundImage = findViewById(R.id.movie_details_background_iv);
+        Picasso.with(this)
+                .load(imageThumbnail)
+                .into(backgroundImage);
+        backgroundImage.setAlpha(0.15f);
     }
 }
