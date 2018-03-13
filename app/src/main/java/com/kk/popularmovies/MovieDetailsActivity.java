9f240a55191104a8f505a6a0eb27a1d3eb10b2ef
kwiecien <k.d.kwiecinski@gmail.com>
2018-03-13 21:42:26 +0100

    feat: Add Home as Up button

--- app/src/main/java/com/kk/popularmovies/MovieDetailsActivity.java
+++ app/src/main/java/com/kk/popularmovies/MovieDetailsActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -45,6 +46,7 @@ public class MovieDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_details);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         supportPostponeEnterTransition();
 
         Intent intent = getIntent();
@@ -80,5 +82,15 @@ public class MovieDetailsActivity extends AppCompatActivity {
 
     private String getReleaseYear(Movie movie) {
         return new SimpleDateFormat("YYYY", Locale.getDefault()).format(movie.getReleaseDate());
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
